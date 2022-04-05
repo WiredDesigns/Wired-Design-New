dff7b8f6749f50bf43647a84aaf04b0b117810d6
WiredDesigns <wireddesignbuisness@gmail.com>
2022-04-05 21:25:27 +0200

    7

--- src/commands/miscellaneous/jokes.js
+++ src/commands/miscellaneous/jokes.js
@@ -1,1 +1,44 @@
-https://www.npmjs.com/package/stamina-api
+const JokeAPI = require('sv443-joke-api');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js')
+
+module.exports = {
+  
+  data: new SlashCommandBuilder()
+  .setName("joke")
+  .setDescription("Sends joke from an API"),
+  async execute(interaction, client) {
+
+
+    
+
+    if(interaction.channel.name.toLowerCase() === "jokes") {
+
+
+
+    const member = interaction.member
+
+
+
+    const res = await (await JokeAPI.getJokes()).json({
+      safeMode: true
+    })
+
+    let fields = []
+    if(res.setup && res.delivery) fields.push({ name: res.setup, value: res.delivery });
+    else fields.push({ name: res.joke, value: `_ _` })
+      const embed = new MessageEmbed()
+      .setTitle(`${res.category} joke`)
+      .setDescription(`${res.type}\n${!res.delivery?res.setup:""}`)
+      .addFields(...fields)
+      .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
+      .setFooter("Made by Wired Design#1487 with ❤️   Api by: Sv443")
+    interaction.reply({embeds: [embed]})
+
+    } else {
+      interaction.reply(`You executed the command in the wrong channel, please execute it in #memes`)
+    }
+    
+
+  }
+}
---
+++ src/commands/miscellaneous/suggest.js
@@ -0,0 +1,51 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+
+const ms = require("ms")
+
+module.exports = {
+ data: new SlashCommandBuilder()   
+ .setName("suggest")
+ .setDescription("You can suggest")
+ .addStringOption((option) => option.setName("suggestion").setDescription('What you want to suggest').setRequired(true)),
+ async execute(interaction, client) {
+  
+  const guild = interaction.guild;
+   
+   const channelname = "suggestions"
+      if(guild.channels.cache.find(c => c.name.toLowerCase() === channelname)){
+        const member = interaction.member
+        let suggestion = interaction.options.getString('suggestion')
+      
+        const channel = member.guild.channels.cache.find(channel => channel.name == channelname)
+      
+
+        const embed = new MessageEmbed()
+          .setTitle(`${interaction.user.username}#${interaction.user.discriminator} has sent a Suggestion`)
+          .setDescription(`${suggestion}`)
+          .setThumbnail(`${interaction.user.displayAvatarURL()}`)
+          .setTimestamp()
+          .setFooter("Created by Wired Design with ❤️")
+          .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
+
+
+  
+                  
+            const message = await channel.send({ embeds: [embed] })
+            message.react("👍")
+            message.react("👎")
+
+
+
+        const successmsg = "Sent the suggestion to " + interaction.guild.channels.cache.find(channel => channel.name === channelname).toString();
+    
+
+        
+        interaction.reply({content: `${successmsg}`})
+      } else {
+
+        const channelcreatemsg = "Create a channel called ``" + channelname + "``" 
+        interaction.reply({content: `${channelcreatemsg}`})
+      }
+ }
+}
