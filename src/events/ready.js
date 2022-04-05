64f1c16f5b2be869fcd568f13188f3e7fa07ed3a
WiredDesigns <wireddesignbuisness@gmail.com>
2022-04-05 21:47:21 +0200

    8

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,40 +3,16 @@ module.exports = {
     once: true,
     execute(client) {
 
-        const statusArray = [
-            {
-                type: 'PLAYING',
-                content: 'Fortnite',
-                status: 'online'
-            },
-            {
-                type: 'LISTENING',
-                content: 'Wired Design on Spotify',
-                status: 'idle'
-            }
-        ];
-
-        async function pickPresence() {
-            const option = Math.floor(Math.random() * statusArray.length)
-
-            try {
-                await client.user.setPresence({
-                    activities: [
-                       {
-                        name: statusArray[option].content,
-                        type: statusArray[option].type
-                       }
-
-                    ],
-                })
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        const option = Math.floor(Math.random() * statusArray.length)
-
-
-        setInterval(pickPresence, 8 * 1000);
+  
+  
+    client.user.setPresence({
+        activities: [
+           {
+            name: "/ Commands",
+            type: "WATCHING"
+           },
+        ],
+    })
         
         console.log(`I started up as ${client.user.username}#${client.user.discriminator}!`);
     }
