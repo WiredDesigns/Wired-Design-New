35634b821c682c3ddc9b053b2bb451fe7c895443
WiredDesigns <wireddesignbuisness@gmail.com>
2022-04-16 18:09:40 +0200

    Update on rank system

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,17 +3,26 @@ module.exports = {
     once: true,
     execute(client) {
 
-  
-  
+  const Guilds = client.guilds.cache.map(guild => guild.id);
     client.user.setPresence({
         activities: [
            {
-            name: "/ commands",
-            type: "PLAYING"
+            name: `/ Commands`,
+            type: "WATCHING"
            },
         ],
     })
-        
+        console.log("===============================================================================")
+        console.log("")
+        console.log("    :::      :::::::: ::::::::::: ::::::::::: :::::::::   ::::::::  ::::    ::: ")
+        console.log("  :+: :+:   :+:    :+:    :+:         :+:     :+:    :+: :+:    :+: :+:+:   :+: ")
+        console.log(" +:+   +:+  +:+           +:+         +:+     +:+    +:+ +:+    +:+ :+:+:+  +:+")
+        console.log("+#++:++#++: :#:           +#+         +#+     +#++:++#:  +#+    +:+ +#+ +:+ +#+ ")
+        console.log("+#+     +#+ +#+   +#+#    +#+         +#+     +#+    +#+ +#+    +#+ +#+  +#+#+#")
+        console.log("#+#     #+# #+#    #+#    #+#         #+#     #+#    #+# #+#    #+# #+#   #+#+#")
+        console.log("###     ###  ######## ###########     ###     ###    ###  ########  ###    ####")
+        console.log("")
+        console.log("===============================================================================")
         console.log(`I started up as ${client.user.username}#${client.user.discriminator}!`);
     }
 }
